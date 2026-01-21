0d96468ba23810e7dd1a345c79f51a56006e5416
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-01-21 12:46:23 +0100

    Fix #54, Fix #56

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,8 @@ details.addEventListener("click", () => {
     document.querySelector("header").style.alignItems = 'flex-start';
 });
 
-document.querySelector("#convertButton").addEventListener("click", () => {
+document.querySelector("#convertButton").addEventListener("click", function (event) {
+    event.preventDefault()
     document.querySelector("#addToLibrary").classList.add("active");
-
-    // document.querySelector("#addToLibrary").style.backgroundColor = '#ea684a';
     document.querySelector("iframe").style.opacity = '100';
-    // document.querySelector("#convertButton").style.backgroundColor = '#42434d';
 });
