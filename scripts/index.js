cedbe2a79d252e9ae79393f9a559c449fd0d6a70
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-01-22 09:02:47 +0100

    Fix #62

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,10 @@ details.addEventListener("click", () => {
     document.querySelector("header").style.alignItems = 'flex-start';
 });
 
-// stap 1: click event + listener op de convert knop
 document.querySelector("#convertButton").addEventListener("click", function (event) {
-    // stap 2: variabele van de textarea ophalen
     const asciiInput = document.querySelector("#ascii");
     console.log(asciiInput);
-    // stap 3: als de input niet leeg is !=, voer if else uit
     if (asciiInput.value !== "") {
-        // voorkomt submit functie
         event.preventDefault();
         document.querySelector("#result").style.display= "block";
     }
